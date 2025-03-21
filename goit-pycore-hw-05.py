383a8312780a7ca80e1ad384e2a4a44c1ae9b6f7
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2025-03-21 14:32:15 +0200

    Update goit-pycore-hw-05.py

--- goit-pycore-hw-05.py
+++ goit-pycore-hw-05.py
@@ -61,16 +61,12 @@ def input_error(func):
         
 @input_error
 def add_contact(args, contacts):
-    if len(args) < 2: 
-        return 'Not enough info'
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
 @input_error
 def change_phone(args, contacts):
-    if len(args) < 2: 
-        return 'Not enough info'
     name, phone = args
     if name in contacts:
         contacts[name] = phone
